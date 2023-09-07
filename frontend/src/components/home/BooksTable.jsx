d93d947b671111fc15465dddb3d5f6d5002a8729
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-09-07 18:36:07 +0530

    added mobile screen compatibility

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -38,7 +38,7 @@ const BooksTable = ({ books }) => {
                         <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
                             {book.publishYear}
                         </td>
-                        <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                        <td className='border border-slate-700 rounded-md text-center'>
                             <div className='flex justify-center gap-x-4'>
                                 <Link to={`/books/details/${book._id}`}>
                                     <BsInfoCircle className='text-2xl text-green-800' />
