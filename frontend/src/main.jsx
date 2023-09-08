78a5ff62b08d4b682b399b5e7709cb73e397d09b
Siddhant Pandey <pandeysiddhant21@gmail.com>
2023-09-09 00:24:10 +0530

    added vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { inject } from '@vercel/analytics';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
